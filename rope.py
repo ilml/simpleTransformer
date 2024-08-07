37e6577532308e69800368568dd517c9262b4bad
Tom Long <mytomlong@gmail.com>
2024-08-07 06:07:19 +0000

    s

--- rope.py
+++ rope.py
@@ -7,15 +7,19 @@ def positional_encoding(x):
     B, S, H = x.shape
     pe = torch.zeros(S, H)
     i = torch.arange(0, H, 2).float()  #[1, H//2]
-    pos = torch.arange(S).unsqueeze(1).float()  #[S, 1]
+    pos = torch.arange(S).float()  #[S, 1]
     div = torch.exp(-math.log(1e4) / H * i)
-    pe[:, 0::2] = torch.sin(pos*div)
-    pe[:, 1::2] = torch.cos(pos*div)
+    angle = torch.outer(pos,div)
+    pe[:, 0::2] = torch.sin(angle)
+    pe[:, 1::2] = torch.cos(angle)
     return pe
 
+
+    
 
 
 if __name__ == "__main__":
     x = torch.ones(4,4,4)
     pe = positional_encoding(x)
+    print(pe)
      
