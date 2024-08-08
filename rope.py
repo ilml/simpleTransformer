04b39aedd6a0232fff90720943eea311bdbfce73
Tom Long <mytomlong@gmail.com>
2024-08-08 20:13:43 +0000

    ok

--- rope.py
+++ rope.py
@@ -15,11 +15,10 @@ def positional_encoding(x):
     return pe
 
 
-
 def precompute_freqs_cis(dim: int, seq_len: int, theta: float = 10000.0): 
-    theta = torch.exp(math.log(1e4) * torch.arange(0, dim, 2)[:dim//2] / dim)
+    t = torch.exp(math.log(theta) * torch.arange(0, dim, 2)[:dim//2] / dim)
     m = torch.arange(seq_len)
-    freqs = torch.outer(m, theta).float()
+    freqs = torch.outer(m, t).float()
     freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_cis
 
@@ -28,18 +27,27 @@ def apply_rotary_emb(
     xq: torch.Tensor,
     xk: torch.Tensor,
     freqs_cis: torch.Tensor,
-) -> Tuple[torch.Tensor, torch.Tensor]:
-    xq_ = xq.view(*xq.shape[::-1], -1, 2)
-    xk_ = xq.view(*xk.shape[::-1], -1, 2)
+) -> tuple[torch.Tensor, torch.Tensor]:
+    xq_ = xq.view(*xq.shape[:-1], -1, 2)
+    xk_ = xk.view(*xk.shape[:-1], -1, 2)
 
-    xq_ = xq_.view_as_complex()
-    xk_ = xk_.view_as_complex()
+    xq_ = torch.view_as_complex(xq_)
+    xk_ = torch.view_as_complex(xk_)
 
-
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(2)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(2)
+    
+    return xq_out, xk_out
 
 
 if __name__ == "__main__":
+    """
     x = torch.ones(4,4,4)
     pe = positional_encoding(x)
     print(pe)
-     
+    """
+    xq = torch.rand([4,4,4])
+    xk = torch.rand([4,4,4])
+    freqs_cis = precompute_freqs_cis(4,4)
+    rope = apply_rotary_emb(xq,xk,freqs_cis)
+    print(rope)
