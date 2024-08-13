22d1a61eb08a8838db28d4d24b8c40a6cb271589
Tom Long <mytomlong@gmail.com>
2024-08-13 04:56:10 +0000

    add mask

--- attention.py
+++ attention.py
@@ -16,7 +16,7 @@ class attention(nn.Module):
         self.head = head
 
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
         batch_size, seq_len, dim = x.shape
         head_dim = dim // self.head
         xq = self.wq(x).view(batch_size, seq_len, self.head, head_dim).transpose(1, 2)
@@ -24,6 +24,8 @@ class attention(nn.Module):
         xv = self.wv(x).view(batch_size, seq_len, self.head, head_dim).transpose(1, 2)
 
         score = xq @ xk.transpose(2, 3) / math.sqrt(head_dim)
+        if mask is not None:
+            score += mask
         attn_weights = F.softmax(score, dim = -1).type_as(xq)
         output = (attn_weights @ xv).transpose(1, 2).contiguous().view(batch_size, seq_len, dim)
         output = self.wo(output)
@@ -37,7 +39,7 @@ if __name__ == "__main__":
     dim = 32
     head = 8
     x = torch.rand(batch_size, seq_len, dim)
+    mask = torch.triu(torch.ones(seq_len, seq_len) * float('-inf'), diagonal=1)
     attention_layer = attention(dim, head)
-    output = attention_layer(x)
-    print(output.shape)
+    output = attention_layer(x, mask)
 
